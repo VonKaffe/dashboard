5b1acbac357548871619f526b4d6c40e32c4564b
Simon Appeltofft <simonappeltofft@hotmail.com>
2018-10-09 12:47:34 +0200

    Added Toolbar

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Toolbar from './Toolbar.js';
+import './App.css';
+import { Button } from 'reactstrap';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <Toolbar/>
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Toolbar.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+
+export default class toolbar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.state = {
+      collapsed: true
+    };
+  }
+
+  toggleNavbar() {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="dark" light>
+          <NavbarBrand href="/" className="mr-auto">Digital Status Dashboard</NavbarBrand>
+          <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+          <Collapse isOpen={!this.state.collapsed} navbar>
+            <Nav navbar>
+              <NavItem>
+                <NavLink href="#">Dashboard</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#">Chart</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#">Sverige Biltema</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
