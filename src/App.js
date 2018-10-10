f963ace87e0d6c8183154996f894e6efcd4c6092
Simon Appeltofft <simonappeltofft@hotmail.com>
2018-10-10 12:28:43 +0200

    Added a grid system for sorting the graphs, needs more work

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Toolbar from './Toolbar.js';
+import { Container, Row, Col } from 'reactstrap'; 
 import './App.css';
+import Dashboard from './Dashboard.js';
 import { Button } from 'reactstrap';
 
 class App extends Component {
